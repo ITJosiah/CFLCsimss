ea3c5a4d81cc2ec10a0f8d640e82ff64b4868e94
Azra3lll <rjdelosreyes39@gmail.com>
2025-11-22 02:53:13 +0800

    heloooo

--- CFLC/AdminManageTeacher.vb
+++ CFLC/AdminManageTeacher.vb
@@ -1,6 +1,9 @@
 ﻿Public Class AdminManageTeacher
     Public Property IsEmbedded As Boolean = False
 
+    Private Sub pnlContent_Paint(sender As Object, e As PaintEventArgs) Handles pnlContent.Paint
+
+    End Sub
 End Class
 
 
