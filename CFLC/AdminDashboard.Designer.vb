4d918c76f6dba9a02de1b5e4aa60461e24119c48
Azra3lll <rjdelosreyes39@gmail.com>
2025-11-21 19:01:19 +0800

    commit dashboard changes

--- CFLC/AdminDashboard.Designer.vb
+++ CFLC/AdminDashboard.Designer.vb
@@ -23,6 +23,8 @@ Partial Class AdminDashboard
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.pnlSidebar = New System.Windows.Forms.Panel()
+        Me.flowButtons = New System.Windows.Forms.FlowLayoutPanel()
         Me.btnManageStudents = New System.Windows.Forms.Button()
         Me.btnManageTeachers = New System.Windows.Forms.Button()
         Me.btnManageSections = New System.Windows.Forms.Button()
@@ -31,98 +33,137 @@ Partial Class AdminDashboard
         Me.btnGenerateReports = New System.Windows.Forms.Button()
         Me.btnLogout = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.pnlSidebar.SuspendLayout()
+        Me.flowButtons.SuspendLayout()
         Me.SuspendLayout()
         '
         'PictureBox1
         '
         Me.PictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None
         Me.PictureBox1.Image = Global.CFLC.My.Resources.Resources.logo
-        Me.PictureBox1.Location = New System.Drawing.Point(512, 124)
+        Me.PictureBox1.Location = New System.Drawing.Point(360, 90)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(375, 330)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 3
         Me.PictureBox1.TabStop = False
         '
+        'pnlSidebar
+        '
+        Me.pnlSidebar.BackColor = System.Drawing.Color.FromArgb(CType(CType(40, Byte), Integer), CType(CType(130, Byte), Integer), CType(CType(61, Byte), Integer))
+        Me.pnlSidebar.Controls.Add(Me.flowButtons)
+        Me.pnlSidebar.Controls.Add(Me.btnLogout)
+        Me.pnlSidebar.Dock = System.Windows.Forms.DockStyle.Left
+        Me.pnlSidebar.Location = New System.Drawing.Point(0, 0)
+        Me.pnlSidebar.Name = "pnlSidebar"
+        Me.pnlSidebar.Padding = New System.Windows.Forms.Padding(30, 40, 30, 40)
+        Me.pnlSidebar.Size = New System.Drawing.Size(300, 633)
+        Me.pnlSidebar.TabIndex = 11
+        '
+        'flowButtons
+        '
+        Me.flowButtons.BackColor = System.Drawing.Color.Transparent
+        Me.flowButtons.Controls.Add(Me.btnManageStudents)
+        Me.flowButtons.Controls.Add(Me.btnManageTeachers)
+        Me.flowButtons.Controls.Add(Me.btnManageSections)
+        Me.flowButtons.Controls.Add(Me.btnManageEnrollments)
+        Me.flowButtons.Controls.Add(Me.btnManageSubjects)
+        Me.flowButtons.Controls.Add(Me.btnGenerateReports)
+        Me.flowButtons.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.flowButtons.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.flowButtons.Location = New System.Drawing.Point(30, 40)
+        Me.flowButtons.Margin = New System.Windows.Forms.Padding(0)
+        Me.flowButtons.Name = "flowButtons"
+        Me.flowButtons.Size = New System.Drawing.Size(240, 503)
+        Me.flowButtons.TabIndex = 0
+        Me.flowButtons.WrapContents = False
+        '
         'btnManageStudents
         '
-        Me.btnManageStudents.Location = New System.Drawing.Point(131, 177)
+        Me.btnManageStudents.Location = New System.Drawing.Point(0, 0)
+        Me.btnManageStudents.Margin = New System.Windows.Forms.Padding(0, 0, 0, 18)
         Me.btnManageStudents.Name = "btnManageStudents"
-        Me.btnManageStudents.Size = New System.Drawing.Size(134, 23)
+        Me.btnManageStudents.Size = New System.Drawing.Size(240, 45)
         Me.btnManageStudents.TabIndex = 4
         Me.btnManageStudents.Text = "Manage Students"
         Me.btnManageStudents.UseVisualStyleBackColor = True
         '
         'btnManageTeachers
         '
-        Me.btnManageTeachers.Location = New System.Drawing.Point(131, 221)
+        Me.btnManageTeachers.Location = New System.Drawing.Point(0, 57)
+        Me.btnManageTeachers.Margin = New System.Windows.Forms.Padding(0, 0, 0, 18)
         Me.btnManageTeachers.Name = "btnManageTeachers"
-        Me.btnManageTeachers.Size = New System.Drawing.Size(134, 23)
+        Me.btnManageTeachers.Size = New System.Drawing.Size(240, 45)
         Me.btnManageTeachers.TabIndex = 5
         Me.btnManageTeachers.Text = "Manage Teachers"
         Me.btnManageTeachers.UseVisualStyleBackColor = True
         '
         'btnManageSections
         '
-        Me.btnManageSections.Location = New System.Drawing.Point(131, 268)
+        Me.btnManageSections.Location = New System.Drawing.Point(0, 114)
+        Me.btnManageSections.Margin = New System.Windows.Forms.Padding(0, 0, 0, 18)
         Me.btnManageSections.Name = "btnManageSections"
-        Me.btnManageSections.Size = New System.Drawing.Size(134, 23)
+        Me.btnManageSections.Size = New System.Drawing.Size(240, 45)
         Me.btnManageSections.TabIndex = 6
         Me.btnManageSections.Text = "Manage Sections"
         Me.btnManageSections.UseVisualStyleBackColor = True
         '
         'btnManageEnrollments
         '
-        Me.btnManageEnrollments.Location = New System.Drawing.Point(131, 314)
+        Me.btnManageEnrollments.Location = New System.Drawing.Point(0, 171)
+        Me.btnManageEnrollments.Margin = New System.Windows.Forms.Padding(0, 0, 0, 18)
         Me.btnManageEnrollments.Name = "btnManageEnrollments"
-        Me.btnManageEnrollments.Size = New System.Drawing.Size(134, 23)
+        Me.btnManageEnrollments.Size = New System.Drawing.Size(240, 45)
         Me.btnManageEnrollments.TabIndex = 7
         Me.btnManageEnrollments.Text = "Manage Enrollments"
         Me.btnManageEnrollments.UseVisualStyleBackColor = True
         '
         'btnManageSubjects
         '
-        Me.btnManageSubjects.Location = New System.Drawing.Point(131, 361)
+        Me.btnManageSubjects.Location = New System.Drawing.Point(0, 228)
+        Me.btnManageSubjects.Margin = New System.Windows.Forms.Padding(0, 0, 0, 18)
         Me.btnManageSubjects.Name = "btnManageSubjects"
-        Me.btnManageSubjects.Size = New System.Drawing.Size(134, 23)
+        Me.btnManageSubjects.Size = New System.Drawing.Size(240, 45)
         Me.btnManageSubjects.TabIndex = 8
         Me.btnManageSubjects.Text = "Manage Subjects"
         Me.btnManageSubjects.UseVisualStyleBackColor = True
         '
         'btnGenerateReports
         '
-        Me.btnGenerateReports.Location = New System.Drawing.Point(131, 418)
+        Me.btnGenerateReports.Location = New System.Drawing.Point(0, 285)
+        Me.btnGenerateReports.Margin = New System.Windows.Forms.Padding(0, 0, 0, 18)
         Me.btnGenerateReports.Name = "btnGenerateReports"
-        Me.btnGenerateReports.Size = New System.Drawing.Size(134, 23)
+        Me.btnGenerateReports.Size = New System.Drawing.Size(240, 45)
         Me.btnGenerateReports.TabIndex = 9
         Me.btnGenerateReports.Text = "Generate Reports"
         Me.btnGenerateReports.UseVisualStyleBackColor = True
         '
         'btnLogout
         '
-        Me.btnLogout.Location = New System.Drawing.Point(131, 472)
+        Me.btnLogout.BackColor = System.Drawing.Color.Red
+        Me.btnLogout.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.btnLogout.ForeColor = System.Drawing.Color.White
+        Me.btnLogout.Location = New System.Drawing.Point(30, 543)
+        Me.btnLogout.Margin = New System.Windows.Forms.Padding(0)
         Me.btnLogout.Name = "btnLogout"
-        Me.btnLogout.Size = New System.Drawing.Size(134, 23)
+        Me.btnLogout.Size = New System.Drawing.Size(240, 50)
         Me.btnLogout.TabIndex = 10
         Me.btnLogout.Text = "Logout"
-        Me.btnLogout.UseVisualStyleBackColor = True
+        Me.btnLogout.UseVisualStyleBackColor = False
         '
         'AdminDashboard
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(15, Byte), Integer), CType(CType(56, Byte), Integer), CType(CType(32, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(1300, 633)
-        Me.Controls.Add(Me.btnLogout)
-        Me.Controls.Add(Me.btnGenerateReports)
-        Me.Controls.Add(Me.btnManageSubjects)
-        Me.Controls.Add(Me.btnManageEnrollments)
-        Me.Controls.Add(Me.btnManageSections)
-        Me.Controls.Add(Me.btnManageTeachers)
-        Me.Controls.Add(Me.btnManageStudents)
+        Me.Controls.Add(Me.pnlSidebar)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "AdminDashboard"
         Me.Text = "AdminDashboard"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.pnlSidebar.ResumeLayout(False)
+        Me.flowButtons.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -135,4 +176,6 @@ Partial Class AdminDashboard
     Friend WithEvents btnGenerateReports As Button
     Friend WithEvents btnLogout As Button
     Friend WithEvents btnManageStudents As Button
+    Friend WithEvents pnlSidebar As Panel
+    Friend WithEvents flowButtons As FlowLayoutPanel
 End Class
