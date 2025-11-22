e34bea351e8390d72a6425bc98fe2e2d2613ddf8
Eloise-Rose <edraphaelcabanela@gmail.com>
2025-11-23 04:17:07 +0800

    cc

--- CFLC/modDB.vb
+++ CFLC/modDB.vb
@@ -4,6 +4,7 @@ Imports System.Security
 Imports System.Security.Cryptography
 Imports ST = System.Runtime.InteropServices
 Imports MySql.Data.MySqlClient
+
 Module modDBx
     Public myadocon, conn As New MySqlConnection
     Public cmd As New MySqlCommand
