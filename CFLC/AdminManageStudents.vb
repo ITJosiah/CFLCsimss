b31ea00cad6927f19ee59c2201049b94a4afe459
Josiah Luke <Josiah Luke@LAPTOP-ITO8MURB>
2025-11-25 21:50:09 +0800

    dsdsdssdsd

--- CFLC/AdminManageStudents.vb
+++ CFLC/AdminManageStudents.vb
@@ -7,36 +7,6 @@ Public Class AdminManageStudents
 
     Public Property IsEmbedded As Boolean = False
 
-    Protected Overrides Function ProcessCmdKey(ByRef msg As Message, keyData As Keys) As Boolean
-        If keyData = Keys.Escape Then
-            ExitFullScreen()
-            Return True
-        End If
-
-        If keyData = Keys.F Then
-            MakeItFullScreen()
-            Return True
-        End If
-
-        Return MyBase.ProcessCmdKey(msg, keyData)
-
-
-    End Function
-
-    Private Sub ExitFullScreen()
-        Me.FormBorderStyle = FormBorderStyle.Sizable
-        Me.WindowState = FormWindowState.Maximized
-        Me.TopMost = False
-    End Sub
-
-    Private Sub MakeItFullScreen()
-        Me.FormBorderStyle = FormBorderStyle.None
-        Me.WindowState = FormWindowState.Maximized
-        Me.Bounds = Screen.PrimaryScreen.Bounds
-        Me.TopMost = True
-        Me.BringToFront()
-    End Sub
-
     Private Sub AdminManageStudents_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         If Not IsEmbedded Then
