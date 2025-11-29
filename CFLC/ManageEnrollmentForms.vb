49a05dbac68e63d93010c242f0d134f6ac450a89
Oengu <Oengu@HUAWHY>
2025-11-30 05:56:41 +0800

    comm staged lng

--- CFLC/ManageEnrollmentForms.vb
+++ CFLC/ManageEnrollmentForms.vb
@@ -1136,7 +1136,5 @@ Public Class ManageEnrollmentForms
         End Try
     End Sub
 
-    Private Sub pnlContent_Paint(sender As Object, e As PaintEventArgs) Handles pnlContent.Paint
 
-    End Sub
 End Class
